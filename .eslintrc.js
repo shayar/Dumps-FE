8273051bf7d5504f352fcc851da783f1737b708d
Susom <susomshrestha@gmail.com>
2024-11-21 11:15:56 -0500

    test new eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,39 +12,25 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime', // Add this for React 17+ without importing React
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', 'jsx-a11y', 'import', '@typescript-eslint'],
+  plugins: [
+    'react', 
+    'react-hooks', 
+    '@typescript-eslint',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   rules: {
-    'no-console': 'error',
-    'no-unused-vars': 'off',
+    'no-console': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
-    'import/first': 'error',
-    'react/prop-types': 0,
-    'prettier/prettier': [
-      'off',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/ban-ts-comment': 2,
-    '@typescript-eslint/no-explicit-any': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 };
