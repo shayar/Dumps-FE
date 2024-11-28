3f6fd91373cacf1e14a05b30e5eab767b8e2be10
Susom <susomshrestha@gmail.com>
2024-11-28 11:13:55 -0500

    remove eslint and husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite';
 import path from 'path';
-import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    checker({ eslint: { lintCommand: 'eslint src' }, overlay: false }),
   ],
   resolve: {
     alias: {
