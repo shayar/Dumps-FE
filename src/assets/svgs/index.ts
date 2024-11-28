2942b771081bfb96f116c432a8a6b78d0bd5aa3b
Susom <susomshrestha@gmail.com>
2024-11-28 14:36:32 -0500

    upgrade packages other than chakra ui

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -8,10 +8,19 @@
  where filename also has a trailing Icon at the end of its name
  is not considered a good approach due to its redundancy and less readability
 */
-export { ReactComponent as DashboardIcon } from './dashboard.svg';
-export { ReactComponent as LogoutIcon } from './logout.svg';
-export { ReactComponent as RightArrowIcon } from './rightArrow.svg';
-export { ReactComponent as LoginIcon } from './sikaaiLogin.svg';
-export { ReactComponent as NotificationIcon } from './notification.svg';
-export { ReactComponent as DumpIcon } from './dump.svg';
-export { ReactComponent as BundleIcon } from './bundle.svg';
+import DashboardIcon from './dashboard.svg?react';
+import LogoutIcon from './logout.svg?react';
+import RightArrowIcon from './rightArrow.svg?react';
+import LoginIcon from './sikaaiLogin.svg?react';
+import NotificationIcon from './notification.svg?react';
+import DumpIcon from './dump.svg?react';
+import BundleIcon from './bundle.svg?react';
+export {
+  DashboardIcon,
+  LoginIcon,
+  LogoutIcon,
+  RightArrowIcon,
+  NotificationIcon,
+  DumpIcon,
+  BundleIcon,
+};
